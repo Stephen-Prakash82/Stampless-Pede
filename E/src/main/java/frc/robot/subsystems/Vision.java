cb256c3b803fff95393a511d443818389503b8e3
ShukkaD <tavish.rajdev1@gmail.com>
2026-03-22 22:29:27 -0400

    minor edits made vision cleaner and added more comments

--- E/src/main/java/frc/robot/subsystems/Vision.java
+++ E/src/main/java/frc/robot/subsystems/Vision.java
@@ -39,10 +39,6 @@ public class Vision extends SubsystemBase {
     // Choose the field layout
     private final AprilTagFieldLayout fieldLayout = GameConstants.kFieldLayout;
 
-    // Variables for Std. Dev
-    private Matrix<N3, N1> estStdDevs;
-    private Matrix<N3, N1> curStdDevs;
-
     // Place the corresponding camera values in the cameraHashMap
     public Vision(SwerveSubsystem swerveDrive) {
 
@@ -65,7 +61,7 @@ public class Vision extends SubsystemBase {
 
         for (Map.Entry<String, cameraStorageObject> entry : cameraHashMap.entrySet()) {
 
-            // Get the cameraStorageObject at the i-th row of the hashmap
+            // Get the cameraStorageObject at entry in the hash-map
             cameraStorageObject cameraKey = entry.getValue();
 
             // Get the pose-estimator and camera stored in the cameraKey
@@ -90,15 +86,20 @@ public class Vision extends SubsystemBase {
                             visionEst = photonEstimator.estimateLowestAmbiguityPose(result);
                         }
 
-                        updateEstimationStdDevs(visionEst, result.getTargets(), photonEstimator);
+                        // Change our trust in the measurement based on the tags we can see
+                        Matrix<N3, N1> estStdDevs = getEstimationStdDevs(visionEst, result.getTargets(),
+                                photonEstimator);
 
                         visionEst.ifPresent(
                                 est -> {
-                                    // Change our trust in the measurement based on the tags we can see
-                                    Matrix<N3, N1> estStdDevs = getEstimationStdDevs();
+
+                                    // Extract the Pose2d out of the vision measurement
                                     Pose2d robotPose = est.estimatedPose.toPose2d();
+
+                                    // Get the timestamp of the vision measurement
                                     double poseTimestamp = est.timestampSeconds;
 
+                                    // Send the vision measurement data to the swerve odometry
                                     m_swerve.swerveDrive.addVisionMeasurement(robotPose, poseTimestamp, estStdDevs);
 
                                 });
@@ -113,11 +114,13 @@ public class Vision extends SubsystemBase {
 
     // USE THIS FUNCTION TO GET THE MOST ACCURATE ROBOT POSE!
     public Pose2d getRobotPose() {
+        // Update our pose-estimate in swerve odometry with latest vision measurement
         getVisionPose();
         return m_swerve.getPose();
     }
 
-    // MAKE SURE TO ONLY USE VAILD TAG-IDs!!!!!!!!
+    // MAKE SURE TO ONLY USE VAILD TAG-IDs BECAUSE THERE IS NO FALLBACK IF OPTIONAL
+    // VALUE FAILS!
     public Pose2d getTagPose(int tagID) {
         return fieldLayout.getTagPose(tagID).get().toPose2d();
     }
@@ -132,7 +135,7 @@ public class Vision extends SubsystemBase {
 
         for (Map.Entry<String, cameraStorageObject> hashMapEntry : cameraHashMap.entrySet()) {
 
-            // Get the cameraStorageObject at the i-th row of the hashmap
+            // Get the cameraStorageObject at entry in the hash-map
             cameraStorageObject cameraKey = hashMapEntry.getValue();
 
             // Get the camera from the cameraStorageObject
@@ -140,34 +143,41 @@ public class Vision extends SubsystemBase {
 
             // Get all unread results
             List<PhotonPipelineResult> latestResults = camera.getAllUnreadResults();
-            double targetIntYaw = 0;
-            // Check if the latest result has April Tags
+
+            // Check if the results have values
             if (!latestResults.isEmpty()) {
-                var latestResult = latestResults.get(latestResults.size() - 1);
 
-                // Get the yaw
-                if (latestResult.hasTargets()) {
-                    // At least one AprilTag was seen by the camera
-                    for (var target : latestResult.getTargets()) {
-                        if (target.getFiducialId() == tagID) {
-                            // Found tag, record its information
-                            targetIntYaw = target.getYaw();
-                            Rotation2d targetYaw = Rotation2d.fromRadians(targetIntYaw);
+                for (PhotonPipelineResult latestResult : latestResults) {
 
-                            // Exit once yaw has been acquired
-                            return Optional.of(targetYaw);
+                    // Check if at least one AprilTag was seen by the camera
+                    if (latestResult.hasTargets()) {
+
+                        for (var target : latestResult.getTargets()) {
+                            // If this is the tag-ID we are looking for
+                            if (target.getFiducialId() == tagID) {
+                                // Found tag, record its information
+                                Rotation2d targetYaw = Rotation2d.fromRadians(target.getYaw());
+
+                                // Exit once yaw has been acquired and return yaw
+                                return Optional.of(targetYaw);
+                            }
                         }
                     }
                 }
             }
         }
+
+        // Return an empty optional if no yaw was acquired
         return Optional.empty();
     }
 
+    // Get a translation from a robot to a point given the distance and yaw to a
+    // point
     public Translation2d robotToPoint(double distanceToPoint, Rotation2d yawToPoint) {
         return PhotonUtils.estimateCameraToTargetTranslation(distanceToPoint, yawToPoint);
     }
 
+    // Get the closest fixed distance from the hub
     public double findClosestRadius(double[] radii, double distance) {
 
         int closest = 0;
@@ -184,9 +194,13 @@ public class Vision extends SubsystemBase {
 
     // Std Dev Calculation from Docs:
     // https://github.com/PhotonVision/photonvision/blob/e8efef476b3b4681c8899a8720774d6dbd5ccf56/photonlib-java-examples/poseest/src/main/java/frc/robot/Robot.java
-    private void updateEstimationStdDevs(
+    private Matrix<N3, N1> getEstimationStdDevs(
             Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets,
             PhotonPoseEstimator poseEstimator) {
+
+        Matrix<N3, N1> estStdDevs;
+        Matrix<N3, N1> curStdDevs;
+
         if (estimatedPose.isEmpty()) {
             // No pose input. Default to single-tag std devs
             curStdDevs = VisionConstants.kSingleTagStdDevs;
@@ -229,12 +243,11 @@ public class Vision extends SubsystemBase {
             }
         }
 
-    }
-
-    private Matrix<N3, N1> getEstimationStdDevs() {
         return curStdDevs;
+
     }
 
+    // Update the swerve odometry with vision measurements periodically
     @Override
     public void periodic() {
         getVisionPose();
